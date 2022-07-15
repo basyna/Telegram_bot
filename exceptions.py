175d84f5369a3a19c8a185ad5e965b88c110cd6d
basyna <86655477+basyna@users.noreply.github.com>
2022-07-15 09:53:48 +0300

    Add files via upload

--- exceptions.py
+++ exceptions.py
@@ -1,16 +1,34 @@
+class ExceptionWithSendingError(Exception):
+    """Ошибка с пересылкой сообщения боту."""
+
+    pass
+
+
+class SendingError(Exception):
+    """Ошибка передачи сообщения боту."""
+
+    pass
+
+
 class EndpointError(Exception):
     """Ошибка при доступе к ендпойнту."""
 
     pass
 
 
-class HomeworksKeyError(Exception):
-    """Отсутствие ключа homeworks."""
+class HomeworksKeyError(ExceptionWithSendingError):
+    """Отсутствие необходимых ключей."""
 
     pass
 
 
-class HomeworksListError(Exception):
+class HomeworksListError(ExceptionWithSendingError):
     """По ключу homeworks возвращается не список."""
+
+    pass
+
+
+class ParseError(ExceptionWithSendingError):
+    """Ошибка при получении данных из работы."""
 
     pass
--- homework.py
+++ homework.py
@@ -2,13 +2,21 @@ import logging
 import os
 import sys
 import time
+from http import HTTPStatus
 from logging import StreamHandler
 
 import requests
 import telegram
 from dotenv import load_dotenv
 
-from exceptions import EndpointError, HomeworksKeyError, HomeworksListError
+from exceptions import (
+    EndpointError,
+    ExceptionWithSendingError,
+    HomeworksKeyError,
+    HomeworksListError,
+    ParseError,
+    SendingError
+)
 
 load_dotenv()
 
@@ -17,12 +25,12 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 15
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
@@ -30,7 +38,7 @@ HOMEWORK_STATUSES = {
 
 
 def set_logging():
-    """Функция формирует и возвращает логгер."""
+    """Функция формирует и возвращает логгер согласно задания."""
     logging.basicConfig(
         format='%(asctime)s, %(levelname)s, %(message)s'
     )
@@ -51,10 +59,11 @@ logger = set_logging()
 
 def send_message(bot, message):
     """Функция отправки сообщения с проверкой."""
+    logger.info(f'Начало отправки сообщения "{message}"')
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
     except Exception as error:
-        logger.error(
+        raise SendingError(
             f'Не удалось отправить сообщение "{message}". Ошибка:{error}'
         )
     else:
@@ -67,29 +76,32 @@ def get_api_answer(current_timestamp):
     """Получение ответа API, проверка статуса ответа."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
+    logger.debug(
+        f'Начале запроса к эндпойнту {ENDPOINT}  с параметром {params}'
+    )
     response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    if response.status_code != 200:
+    if response.status_code != HTTPStatus.OK:
         raise EndpointError(
             f'Сбой в работе программы: Эндпоинт {ENDPOINT} недоступен.'
             f'Код ответа API: {response.status_code}'
         )
     else:
-        return(response.json())
+        return response.json()
 
 
 def check_response(response):
     """Проверка ответа сервера."""
     try:
         hw_list = response['homeworks']
-    except KeyError:
+    except KeyError as error:
         raise HomeworksKeyError(
-            'Ключ homeworks не обнаружен.'
+            f'Ключ {error} не обнаружен.'
         )
     if not isinstance(hw_list, list):
         raise HomeworksListError(
             'Объект homeworks не является списком.'
         )
-    return(hw_list)
+    return hw_list
 
 
 def parse_status(homework):
@@ -97,49 +109,48 @@ def parse_status(homework):
     try:
         homework_status = homework['status']
         homework_name = homework['homework_name']
-        verdict = HOMEWORK_STATUSES[homework_status]
-    except KeyError:
-        raise KeyError(
-            'Ошибка получения данных по ключу в функции parse_status.'
+        verdict = HOMEWORK_VERDICTS[homework_status]
+    except ParseError as error:
+        raise ParseError(
+            f'Ошибка получения данных по ключу "{error}"'
+            f' в функции parse_status.'
         )
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """Функция проверки наличия необходимых данных."""
-    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
-        return True
-    else:
-        return False
+    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
 
 
 def main():
     """Основная логика работы бота."""
     if not check_tokens():
         logger.critical('Токены не доступны. Работа не возможна!')
-        raise SystemExit
+        sys.exit('Токены не доступны. Работа не возможна!')
     current_timestamp = int(time.time())
-    not_ready_to_exit = True
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     last_error = ''
-    while not_ready_to_exit:
+    while True:
         try:
             response = get_api_answer(current_timestamp)
-            last_hw = check_response(response)[0]
-            message = parse_status(last_hw)
-        except IndexError:
-            logger.debug('Нет новых записей')
+            homeworks = check_response(response)
+            if homeworks:
+                message = parse_status(homeworks[0])
+                send_message(bot, message)
+            else:
+                logger.debug('Нет новых записей')
+            current_timestamp = response.get(
+                'current_date', current_timestamp
+            )
+        except ExceptionWithSendingError as error:
+            if error != last_error:
+                send_message(bot, error)
+                last_error = error
+            logger.error(error)
         except Exception as error:
             error_message = f'Сбой в работе программы: {error}'
             logger.error(error_message)
-            if last_error != error:
-                send_message(bot, error_message)
-                last_error = error
-        else:
-            send_message(bot, message)
-            if last_hw['status'] == 'approved':
-                not_ready_to_exit = False
-            current_timestamp = response.get('current_date', current_timestamp)
         finally:
             time.sleep(RETRY_TIME)
 
